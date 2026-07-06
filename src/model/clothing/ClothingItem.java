803ffc7c7b68fa4c2fcebe4c516de037781a6188
kbv851 <kbv851@usask.ca>
2026-07-06 14:09:22 -0600

    Established what ClothingItem should have.

--- src/model/clothing/ClothingItem.java
+++ src/model/clothing/ClothingItem.java
@@ -1,5 +1,43 @@
 package model.clothing;
 
-public class ClothingItem {
+public abstract class ClothingItem {
     
+    protected String id;
+    protected String name;
+    protected String color;
+    protected String brand;
+    protected String imagePath;
+
+    public ClothingItem(String id, String name, String color, String brand, String imagePath){
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.brand = brand;
+        this.imagePath = imagePath;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getColor(){
+        return brand;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public String getImagePath(){
+        return imagePath;
+    }
+
+    @Override
+    public String toString(){
+        return name + "[" + color + "," + brand + "]";
+    }
 }
