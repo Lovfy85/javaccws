c8076289c16c4c09beffa9da023c9b6e17133a9f
kbv851 <kbv851@usask.ca>
2026-07-06 15:38:37 -0600

    Established what StylesProfile should have.

--- src/model/StylesProfile.java
+++ src/model/StylesProfile.java
@@ -2,4 +2,24 @@ package model;
 
 public class StylesProfile {
     
+    private String style; 
+    private String colorPreference;
+
+    public StylesProfile(String style, String colorPreference){
+        this.style = style;
+        this.colorPreference = colorPreference;
+    }
+
+    public String getStyle(){
+        return style;
+    }
+
+    public String getColorPreference(){
+        return colorPreference;
+    }
+
+    @Override
+    public String toString(){
+        return style + " | " + colorPreference;
+    }
 }
