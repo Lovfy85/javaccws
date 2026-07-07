9bbf79b8e5df2aa67d4b932e936e373b788fabbb
kbv851 <kbv851@usask.ca>
2026-07-06 18:43:33 -0600

    For ImageLoader.java, this will let me test out the upload and display interaction of imported images to the application.

--- src/util/ImageLoader.java
+++ src/util/ImageLoader.java
@@ -1,5 +1,21 @@
 package util;
 
+import javax.swing.ImageIcon;
+import java.awt.Image;
+
 public class ImageLoader {
-    
+
+    public static ImageIcon load(String path) {
+
+        ImageIcon icon = new ImageIcon(path);
+
+        Image resized = icon.getImage()
+                .getScaledInstance(
+                        150,
+                        150,
+                        Image.SCALE_SMOOTH
+                );
+
+        return new ImageIcon(resized);
+    }
 }
