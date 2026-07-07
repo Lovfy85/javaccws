bbc86f5c23d442c30764a19e061d3d28002ecae1
kbv851 <kbv851@usask.ca>
2026-07-06 18:17:28 -0600

    Established what Wardrobe  should have.

--- src/model/Wardrobe.java
+++ src/model/Wardrobe.java
@@ -1,5 +1,25 @@
 package model;
 
+import model.clothing.ClothingItem;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wardrobe {
     
+    private List<ClothingItem> items = new ArrayList<>();
+
+    public void addItem(ClothingItem item){
+        items.add(item);
+    }
+
+    public List<ClothingItem> getItems(){
+        return items;
+    }
+
+    public void printWardrobe(){
+        System.out.println("Wardrobe: ");
+        for (ClothingItem item : items){
+            System.out.println("- " + item);
+        }
+    }
 }
